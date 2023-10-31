660fcedb80f8283c7d10ff42ba4b20af2f0364ae
ebalzuweit <ebalzuweit@gmail.com>
2023-10-30 17:32:17 -0700

    Refactor to use range

--- src/components/IterativeFunctionRender.tsx
+++ src/components/IterativeFunctionRender.tsx
@@ -7,18 +7,18 @@ export interface IterativeFunctionProps {
 	xFct: (i: number) => number
 	yFct: (i: number) => number
 	zFct: (i: number) => number
-	iterations?: number
+	from?: number
+	to?: number
 }
-const IterativeFunctionRender: React.FC<IterativeFunctionProps> = ({ xFct, yFct, zFct, iterations = 10_000 }: IterativeFunctionProps) => {
+const IterativeFunctionRender: React.FC<IterativeFunctionProps> = ({ xFct, yFct, zFct, from = -5_000, to = 5_000 }: IterativeFunctionProps) => {
 	const points = useMemo(() => {
 		const points = []
-		const halfIterations = iterations / 2
-		for (let i = -halfIterations; i < halfIterations; i++) {
+		for (let i = from; i < to; i++) {
 			const point = new Vector3(xFct(i), yFct(i), zFct(i))
 			points.push(point)
 		}
 		return points
-	}, [xFct, yFct, zFct, iterations])
+	}, [xFct, yFct, zFct, from, to])
 
 	return (
 		<Line
