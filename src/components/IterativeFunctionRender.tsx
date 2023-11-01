601014113f6958e190447d14d117122f0721cddc
ebalzuweit <ebalzuweit@gmail.com>
2023-10-31 21:11:47 -0700

    Custom step size for renders

--- src/components/IterativeFunctionRender.tsx
+++ src/components/IterativeFunctionRender.tsx
@@ -4,19 +4,21 @@ import IterativeFunction from '../math/IterativeFunction'
 
 export interface IterativeFunctionProps {
 	func: IterativeFunction
+	color?: string
 	from?: number
 	to?: number
-	color?: string
+	step_size?: number
 }
 const IterativeFunctionRender: React.FC<IterativeFunctionProps> = ({
 	func,
+	color = "black",
 	from = -5_000,
 	to = 5_000,
-	color = "black"
+	step_size = 0.1
 }: IterativeFunctionProps) => {
 	const points = useMemo(() => {
 		const points = []
-		for (let i = from; i < to; i++) {
+		for (let i = from; i < to; i += step_size) {
 			const point = func.evaluate(i)
 			points.push(point)
 		}
